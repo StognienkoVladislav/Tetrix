941c76ead6c53f8bf5864e7b27776526310b2ad3
stognienkovladislav <stognienkovv@gmail.com>
2017-10-08 01:35:11 +0300

    trash3

--- test_script.py
+++ test_script.py
@@ -1,11 +1,7 @@
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from pandas import DataFrame
-from pandas import Series
-
-
+import pickle
 
 df = pd.read_csv("PR-41713 product_attributes_train.csv", error_bad_lines=False,
                  names  = ['id', 'attribute_name',
@@ -34,16 +30,19 @@ i=0
 for x in lst2["attribute_name"].unique():
     lst3[x] = {}
 
-for x, y in zip(lst2["attribute_name"].unique(), df[1:]["attribute_value"]):
 
-    for z in df[1:]["attribute_name"]:
-        if x == z:
-            if y in lst3[x]:
-                lst3[x][y] += 1
+
+for x in lst2["attribute_name"].unique():
+
+    for k, v in zip(lst2["attribute_name"], lst2["attribute_value"]):
+        if x == k:
+            if v in lst3[x]:
+                lst3[x][v] += 1
             else:
-                lst3[x][y] = 1
+                lst3[x][v] = 1
 
 
 print(lst3)
 
-print("HUI")
+df2 = DataFrame(lst3)
+print(df2)
